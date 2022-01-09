7a29e0b64596bd1fd8d15cbb7978ab395a50648b
Mukka Deepak <ramu92013@gmail.com>
2022-01-09 18:52:57 +0530

    Updates

---
+++ precache-manifest.b01bfa84d5ea36c8936a3e83e8b62c7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d3f9957e611eeee40cd2761d880d05d",
+    "url": "/movie-save/index.html"
+  },
+  {
+    "revision": "28276376b64c1d42515f",
+    "url": "/movie-save/static/css/main.653e6977.chunk.css"
+  },
+  {
+    "revision": "ac12698f8dc1a63406b4",
+    "url": "/movie-save/static/js/2.70d92d32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-save/static/js/2.70d92d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28276376b64c1d42515f",
+    "url": "/movie-save/static/js/main.5f2b2ae9.chunk.js"
+  },
+  {
+    "revision": "46c889763cc1fad495a2",
+    "url": "/movie-save/static/js/runtime-main.8c811c44.js"
+  }
+]);
